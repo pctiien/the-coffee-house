b0cb2de7ceefb6ee3b17f286f296a7a818ced9a3
pctien <pctien1401@gmail.com>
2024-10-01 13:03:37 +0700

    handle item click navbar

--- client/admin/src/features/NavBar/NavBarItem.jsx
+++ client/admin/src/features/NavBar/NavBarItem.jsx
@@ -1,9 +1,34 @@
-const NavBarItem = ({title,imgSrc,subItem})=>{
+import React from 'react'
+
+const NavBarItem = ({isOpen,title,imgSrc,subItems})=>{
+
+    const [selectedItem,setSelectedItem] = React.useState(null)
+    
+    const handleClick = (e,index)=>{
+        e.stopPropagation()
+        setSelectedItem(index)
+    }
+
     return (
         <>
-            <div className=" cursor-pointer flex gap-2 items-center group ">
-                <img className="w-6 h-6 group-hover:text-blue-600" src={imgSrc} alt="" />
-                <h1 className="group-hover:text-blue-600">{title}</h1>
+            <div>
+                <div className=" cursor-pointer flex gap-2 items-center group ">
+                    <img className="w-6 h-6 group-hover:text-blue-600" src={imgSrc} alt="" />
+                    <h1 className="group-hover:text-blue-600 text-base">{title}</h1>
+                </div>
+                <div className = {` ${ isOpen ? 'opacity-100 visible' : 'hidden'}`}>
+                    <ul className = 'list-disc list-inside p-2'>
+                        {
+                            subItems?.map((item,index)=>{
+                                return (
+                                    <li 
+                                    onClick={(e) => handleClick(e,index)}
+                                    key={index} className={`p-1 cursor-pointer hover:text-blue-400 text-base ${selectedItem === index ? 'text-blue-400': ''}`}>{item.title}</li>
+                                )
+                            })
+                        }
+                    </ul>
+                </div>
             </div>
         </>
     )
--- client/admin/src/ui/NavBar.jsx
+++ client/admin/src/ui/NavBar.jsx
@@ -1,17 +1,96 @@
 import NavBarItem from '../features/NavBar/NavBarItem'
-
+import React from 'react'
 const NavBar = () => {
+
+    const [navState, setNavState] = React.useState([
+        {
+            title: 'Product',
+            icon: './cart.png',
+            subItems: [
+                {
+                    id: 1,
+                    title: 'Add Product',
+                    ref: '#'
+                },
+                {
+                    id: 2,
+                    title: 'Product List',
+                    ref: '#'
+                },
+            ]
+        },
+        {
+            title: 'Category',
+            icon: './category.png',
+            subItems: [
+                {
+                    id: 1,
+                    title: 'Category List',
+                    ref: '#'
+                },
+                {
+                    id: 2,
+                    title: 'New Category',
+                    ref: '#'
+                },
+            ]
+        },
+        {
+            title: 'Order',
+            icon: './order.png',
+            subItems: [
+                {
+                    id: 1,
+                    title: 'Order List',
+                    ref: '#'
+                },
+                {
+                    id: 2,
+                    title: 'Order Detail',
+                    ref: '#'
+                },
+            ]
+        },
+        {
+            title: 'User',
+            icon: './user.png',
+            subItems: [
+                {
+                    id: 1,
+                    title: 'All User',
+                    ref: '#'
+                }
+            ]
+        }
+    ]);
+    
+    const [selectedItem,setSelectedItem] = React.useState(null)
+
+    const handleClick = (index) => {
+        if(selectedItem === index) return setSelectedItem(null) 
+        setSelectedItem(index)
+    };
+    
     return (
         <div className="p-10 shadow-2xl w-1/6 h-screen ">
             <div className="flex flex-col gap-5 text-sm font-semibold justify-center">
                 <h1 className='uppercase text-gray-400 font-semibold text-sm'>All pages</h1>
-                <NavBarItem title ='Product' imgSrc = './cart.png' subItem= {[]}/>
-                <NavBarItem title ='Category' imgSrc = './category.png' subItem= {[]}/>
-                <NavBarItem title ='Order' imgSrc = './order.png' subItem= {[]}/>
-                <NavBarItem title ='User' imgSrc = './user.png' subItem= {[]}/>
+                {
+                    navState?.map((item,index)=>{
+                        return (
+                            <div key={index}
+                            onClick={()=>handleClick(index)}>
+                                <NavBarItem 
+                                isOpen={selectedItem === index}
+                                title ={navState[index].title} imgSrc = {navState[index].icon}  subItems= {navState[index].subItems}/>  
+                            </div>
+                        )
+                    })
+                }
+                
 
             </div>
-        </div>
+        </div>  
     );
 };
 
